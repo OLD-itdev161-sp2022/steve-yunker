cc81639271a040a665fd50f0a7c069c9c01fc254
steve yunker <yunkers@gmatc.matc.edu>
2022-02-10 12:29:14 -0600

    created user registration api endpoint

--- server.js
+++ server.js
@@ -5,8 +5,27 @@ const app = express();
 
 connectDatabase();
 
+app.use(express.json({ extended: false}));
+
+//api endpoints
+/**
+ * @route GET /
+ * @desc Test endpoint
+ * 
+ */
+
 app.get('/', (req, res) =>
     res.send('http get request sent to root api endpoint')
 );
+
+/** 
+* @route POST api/users
+* @desc Register user
+* 
+*/
+app.post('/api/users', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
 
 app.listen(3000, () => console.log('Express server running on port 3000'));
