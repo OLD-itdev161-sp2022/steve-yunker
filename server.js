2a5b677b2a10305fbbab4c7a3e6eb68754a0c755
steve yunker <yunkers@gmatc.matc.edu>
2022-03-04 12:17:17 -0600

    Completed Activity 5

--- server.js
+++ server.js
@@ -33,44 +33,67 @@ app.get('/', (req, res) =>
 * @desc Register user
 * 
 */
-app.post('/api/users',
-[
-    check('name', 'Please enter your name').not().isEmpty(),
-    check('email', 'Please enter a valid email').isEmail(),
-    check('password', 'Please enter a password with 6 or more characters').isLength({min:6})
-],
-async (req,res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()){
-        return res.status(422).json({errors: errors.array() });
-    } else{
-        const {name, email, password } = req.body;
-        try {
-            //Check if user exists
-            let user = await User.findOne({ email: email});
-            if (user) {
-                return res
-                    .status(400)
-                    .json({ errors: [{ msg: 'User already exists'}] });
+app.post(
+    '/api/users',
+    [
+        check('name', 'Please enter your name')
+            .not()
+            .isEmpty(),
+        check('email', 'Please enter a valid email').isEmail(),
+        check(
+            'password',
+            'Please enter a password with 6 or more characters'
+        ).isLength({ min: 6 })
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        } else {
+            const { name, email, password } = req.body;
+            try {
+                // Check if user exists
+                let user = await User.findOne({ email: email });
+                if (user) {
+                    return res
+                        .status(400)
+                        .json({ errors: [{ msg: 'User already exists' }] });
+                }
+
+                // Create a new user
+                user = new User({
+                    name: name,
+                    email: email,
+                    password: password
+                });
+
+                // Encrypt the password
+                const salt = await bcrypt.genSalt(10);
+                user.password = await bcrypt.hash(password, salt);
+
+                // Save to the db and return
+                await user.save();
+
+                //Generate and return a JWT token
+                const payload = {
+                    user: {
+                        id: user.id
+                    }
+                };
+
+                jwt.sign(
+                    payload,
+                    config.get('jwtSecret'),
+                    { expiresIn: '10hr' },
+                    (err, token) => {
+                        if (err) throw err;
+                        res.json({ token: token });
+                    }
+                );
+            } catch (error) {
+                res.status(500).send('Server error');
             }
 
-            //create new user
-            user = new User({
-                name: name,
-                email: email,
-                password: password
-            });
-
-            //Encrypt the password
-            const salt = await bcrypt.genSalt(10);
-            user.password = await bcrypt.hash(password, salt);
-
-            //save to the db and return
-            await user.save();
-            res.send('User successfully registered');
-        } catch (error) {
-            res.status(500).send('Server error')
-            }
         }
     }
 );
